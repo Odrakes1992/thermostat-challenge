aa9fa17762539fd496b456712c9442f9d48d9e6d
Osneil Drakes <osneil-drakes@hotmail.com>
2021-09-30 09:18:35 +0100

    updating logic

---
+++ src/thermostat.js
@@ -0,0 +1,58 @@
+'use strict';
+
+class Thermostat {
+  constructor() {
+    this.temperature = 20;
+    this.MINIMUM_TEMPERATURE = 10;
+    this.powerSavingMode = true;
+    this.MAX_LIMIT_PSM_ON = 25;
+    this.MAX_LIMIT_PSM_OFF = 32;
+  }
+  
+  getCurrentTemperature() {
+    return this.temperature;
+  }
+
+  up() {
+    if (this.isMaximumTemperature()) {
+      return;
+       // If our current temperature is already set to the max limit i.e true, 
+      // we do nothing - otherwise we turn it up
+    }
+    this.temperature += 1
+  }
+
+  down() {
+    if (this.isMinimumTemperature()) {
+      return;
+      // If our current temperature is already set to the min limit i.e true, 
+      // we do nothing - otherwise we turn it down
+    }
+    this.temperature -= 1
+  }
+
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+
+  isPowerSavingModeOn() {
+    return this.powerSavingMode === true;
+  }
+
+  switchPowerSavingModeOff() {
+    this.powerSavingMode = false;
+  }
+  
+  switchPowerSavingModeOn() {
+    this.powerSavingMode = true;
+  }
+
+  isMaximumTemperature() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.MAX_LIMIT_PSM_OFF;
+    }
+    return this.temperature === this.MAX_LIMIT_PSM_ON;
+  }
+
+
+};
