5a6cd7fa9b2b98bd0c4651dac70ff80d6d3de5e4
Osneil Drakes <osneil-drakes@hotmail.com>
2021-09-30 09:27:16 +0100

    complete thermostat logic

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,11 +2,14 @@
 
 class Thermostat {
   constructor() {
-    this.temperature = 20;
+    this.DEFAULT_TEMPERATURE = 20;
+    this.temperature = this.DEFAULT_TEMPERATURE;
     this.MINIMUM_TEMPERATURE = 10;
     this.powerSavingMode = true;
     this.MAX_LIMIT_PSM_ON = 25;
     this.MAX_LIMIT_PSM_OFF = 32;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
+    this.HIGH_ENERGY_USAGE_LIMIT = 25;
   }
   
   getCurrentTemperature() {
@@ -54,5 +57,18 @@ class Thermostat {
     return this.temperature === this.MAX_LIMIT_PSM_ON;
   }
 
+  resetTemperature() {
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  }
+
+  energyUsage() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature <= this.HIGH_ENERGY_USAGE_LIMIT) {
+      return 'medium-usage';
+    }
+    return 'high-usage';
+  }
 
 };
