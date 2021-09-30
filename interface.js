920f9c53d73262e5b5466175d5c8908d2936aa98
Osneil Drakes <osneil-drakes@hotmail.com>
2021-09-30 16:51:08 +0100

    setting up the API

---
+++ interface.js
@@ -0,0 +1,66 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+  const thermostat = new Thermostat();
+  const updateTemperature = () => {
+    document.querySelector('#temperature').innerText = thermostat.temperature;
+    document.querySelector('#temperature').className = thermostat.energyUsage();
+  }
+
+  document.querySelector('#temperature-up').addEventListener('click', () => {
+    thermostat.up();
+    updateTemperature();
+  });
+
+  document.querySelector('#temperature-down').addEventListener('click', () => {
+    thermostat.down();
+    updateTemperature();
+  });
+
+  document.querySelector('#temperature-reset').addEventListener('click', () => {
+    thermostat.resetTemperature();
+    updateTemperature();
+  });
+
+  document.querySelector('#powersaving-on').addEventListener('click', () => {
+    thermostat.switchPowerSavingModeOn();
+    document.querySelector('#power-saving-status').innerText = 'on';
+    updateTemperature();
+  })
+
+  document.querySelector('#powersaving-off').addEventListener('click', () => {
+    thermostat.switchPowerSavingModeOff();
+    document.querySelector('#power-saving-status').innerText = 'off';
+    updateTemperature();
+  })
+
+  // fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric')
+  // .then((response) => {
+  //   return response.json()
+  // })
+  // .then((data) => {
+  //   document.querySelector('#current-temperature').innerText = data.main.temp;
+  // });
+
+  const displayWeather = (city) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+  
+    fetch(url)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        document.querySelector('#current-temperature').innerText = data.main.temp;
+      })
+  }
+  displayWeather('London');
+
+  document.querySelector('#current-city').addEventListener('change', (event) => {
+    event.preventDefault();
+    const city = document.querySelector('#current-city').value;
+  
+    displayWeather(city);
+  })
+
+
+});
